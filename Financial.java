032c0a45b1d3d15c71a7a4dd2e7d132f16a31ef2
am-shm <am.shahmoradi93@gmail.com>
2022-05-13 21:29:16 +0430

    methods base added

--- Financial.java
+++ Financial.java
@@ -1,7 +1,16 @@
 public class Financial {
-	//loan option
-	
-	public void teacherLoan(int ID, int option){
-		
+	//loan part
+	public class Loan {
+		/* loan have some options and plans */
+		public void student(int ID, int option){
+			if ( /* db.student.bedehi.id(ID) */ > 0 ){
+				//error
+			}else {
+				//give lone 
+			}
+		}
+		public void teacher(int ID,int option){
+			
+		}
 	}
 }
