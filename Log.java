87569172f77e5fa1bdacc0f0687a0f1c158e779f
am-shm <am.shahmoradi93@gmail.com>
2022-05-15 22:41:01 +0430

    commect added, loan log struct added

--- Log.java
+++ Log.java
@@ -1,8 +1,13 @@
+// this class will call after every important event to creat a log in a text file for every year
+
 public class Log {
-	String failedTerm(int ID, int class_code, String reason) {
-		//return "$ID: failed class_code by $reason"
+	String failedTerm(int ID, int class_code, String reason, String date) {
+		//return "$ID: failed class_code by $reason on $date"
 	}
-	String passedTerm(int ID, int class_code, int nomre){
-		//return "$ID: passed class_code
+	String passedTerm(int ID, int class_code, int nomre, String date){
+		//return "$ID: passed class_code on $date"
+	}
+	String giveLoan(int ID, int quantity, String date){
+		//return "$ID: give $quantity loan on $date"
 	}
 }
