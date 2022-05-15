3b41a847f558ea7771eba888b0b4429eae694713
am-shm <am.shahmoradi93@gmail.com>
2022-05-15 21:51:36 +0430

    some comment and todo list for UI

--- UI.java
+++ UI.java
@@ -1,4 +1,21 @@
-public class UI {
-	
+/*   TODO_list:
 
+main login frame => student, employee and teacher button
+main page => dashboard [RIGHT] / some info [TOP] / widgets [CENTER]
+
+
+*/
+
+public class UI {
+	public int main_page (){
+		//for teacher choice return 1
+		//for student choice return 2
+		//for employee choice return 3
+	}
+	class loan_UI {
+		// string --> shown from db value
+		// checkbox
+		// submit button
+		// actionListener return choice and call financial methods
+	}
 }
