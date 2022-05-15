39f62792699e1b89d85e5daa8b941030dcd47dff
am-shm <am.shahmoradi93@gmail.com>
2022-05-15 15:29:47 +0430

    some changes happend

---
+++ Tchr.java
@@ -0,0 +1,15 @@
+public class Teacher{
+	public int teacherID;
+	
+	public void viewStatus(int stdnID){
+		// db.show.student.EY.id(stdnID)
+		// db.show.student.major(stdnID)
+	}
+	public void deleteStudent(int stdnID,int lessonCode){
+		//db.Student.delete.stdnID from lessonCode_table
+	}
+	public void grading (int mark, int stdnID, int lessonCode){
+		//db.update
+	}
+	
+}
