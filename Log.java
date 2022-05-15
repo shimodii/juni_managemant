7c44e9fabe992b4eab3d5507c71896de6a77de24
am-shm <am.shahmoradi93@gmail.com>
2022-05-15 15:26:20 +0430

    log class

---
+++ Log.java
@@ -0,0 +1,8 @@
+public class Log {
+	String failedTerm(int ID, int class_code, String reason) {
+		//return "$ID: failed class_code by $reason"
+	}
+	String passedTerm(int ID, int class_code, int nomre){
+		//return "$ID: passed class_code
+	}
+}
