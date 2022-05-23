4f2345ce461af2179036de81550d67d154e9aff0
am-shm <am.shahmoradi93@gmail.com>
2022-05-23 14:28:49 +0430

    main frame developed

---
+++ Final_maybe/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame mainFrame = new JFrame("Login");
+
+		JButton submitButton = new JButton("submit");
+		mainFrame.add(submitButton);
+		mainFrame.setLayout(null);
+		mainFrame.setVisible(true);
+	}
+}
