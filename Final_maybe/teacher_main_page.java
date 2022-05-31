e9ad13cd03bf9a24cd1241e197980c4a320b1a7a
am-shm <am.shahmoradi93@gmail.com>
2022-05-31 15:22:23 +0430

    teachers main page with persian menu DONE

--- Final_maybe/teacher_main_page.java
+++ Final_maybe/teacher_main_page.java
@@ -1,43 +1,55 @@
 import javax.swing.*;
 
 public class Main {
+	//every UI class must called with username that loged-in so:
+	String username;
 	public static void main(String[] args) {
 		//mainFrame 
-		JFrame mainFrame = new JFrame("Login");
+		JFrame mainFrame = new JFrame("صفحه اصلی");
 
 		//variables
-		JMenuBar studentMB = new JMenuBar();
-		JMenu menuTahsili = new JMenu("tahsili");
-		JMenu menuMali = new JMenu("mali");
-		JMenu menuEdari = new JMenu("edari");
-		JMenuItem tahsiliHazf = new JMenuItem("hazf/ezafe");
+		JMenuBar teacherMB = new JMenuBar();
+		JMenu menuTahsili = new JMenu("امور تحصیلی");
+		JMenu menuMali = new JMenu("امور مالی");
+		JMenu menuEdari = new JMenu("امور اداری");
+		JMenu menuExtra = new JMenu("متفرقه");
 		/* --tahsili menu submenus-- */
-		JMenuItem tahsiliEntekhab = new JMenuItem("entekhab");
-		JMenuItem tahsiliMoshahedeD = new JMenuItem("moshahede drus");
-		JMenuItem tahsiliArzeshyabi = new JMenuItem("arzeshyabi ostad");
-		JMenuItem tahsiliMoshahedeV = new JMenuItem("moshahede vaziyat");
-		JMenu tahsiliKarname = new JMenu("karname");
-		/* --karname submenu-- */
-		JMenuItem karnameMoshahede = new JMenuItem("moshahede karname");
-		JMenuItem karnameEteraz = new JMenuItem("eteraz be nomre");
+		JMenuItem tahsiliNomre = new JMenuItem("نمره دهی");
+		JMenuItem tahsiliHazf = new JMenuItem("حذف دانشجو");
+		JMenuItem tahsiliMoshahedeD = new JMenuItem("مشاهده دروس");
+		JMenuItem tahsiliMoshDan = new JMenuItem("مشاهده لیست دانشجویان");
+		/* --mali menu submenu-- */
+		JMenuItem maliVaziyat = new JMenuItem("مشاهده تراز مالی");
+		JMenuItem maliVaam = new JMenuItem("درخواست وام");
+		/* --edari menu submenu-- */
+		JMenuItem edariMorakhasi = new JMenuItem("ثبت مرخصی");
+		/* --extra menu submenu-- */
+		JMenuItem extraProfedit = new JMenuItem("ویرایش پروفایل");
+		JMenuItem extraContact = new JMenuItem("راه های ارتباطی");
+		JMenuItem extraGuide = new JMenuItem("راهنما");
 
 		//adding submenus to menubar
-		studentMB.add(menuTahsili);
-		studentMB.add(menuMali);
-		studentMB.add(menuEdari);
+		teacherMB.add(menuTahsili);
+		teacherMB.add(menuMali);
+		teacherMB.add(menuEdari);
+		teacherMB.add(menuExtra);
 		/* --tahsili submenu-- */
-		menuTahsili.add(tahsiliEntekhab);
+		menuTahsili.add(tahsiliNomre);
 		menuTahsili.add(tahsiliHazf);
 		menuTahsili.add(tahsiliMoshahedeD);
-		menuTahsili.add(tahsiliArzeshyabi);
-		menuTahsili.add(tahsiliMoshahedeV);
-		menuTahsili.add(tahsiliKarname);
-		/* .:karname submenu:. */
-		tahsiliKarname.add(karnameMoshahede);
-		tahsiliKarname.add(karnameEteraz);
+		menuTahsili.add(tahsiliMoshDan);
+		/* --mali submenu-- */
+		menuMali.add(maliVaziyat);
+		menuMali.add(maliVaam);
+		/* --edari submenu-- */
+		menuEdari.add(edariMorakhasi);
+		/* --extra submenu-- */
+		menuExtra.add(extraProfedit);
+		menuExtra.add(extraContact);
+		menuExtra.add(extraGuide);
 
 		//adding elements to mainFrame
-		mainFrame.setJMenuBar(studentMB);
+		mainFrame.setJMenuBar(teacherMB);
 		
 		//mainFrame init
 		mainFrame.setSize(1270,1000);
