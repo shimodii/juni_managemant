b05802c75808375e59678745f2467415c2d2533b
am-shm <am.shahmoradi93@gmail.com>
2022-06-03 13:30:20 +0430

    guide message added to show when guide button clicked

---
+++ Final_maybe/guide_message.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+
+public class Main {
+
+	public static void main (String args[]) {
+		//main frame
+		JFrame mainFrame = new JFrame("راهنما");
+
+		JOptionPane.showMessageDialog(mainFrame,"""
+				باسلام \n
+				نام کاربری و رمز عبور در صورت عدم تغییر برای تمامی دانشجویان عزیز شماره دانشجویی ایشان،\n
+				همچنین برای اساتید و کارکنان محترم شماره پرسنلی ایشان میباشد.\n
+				باری تغییر رمز عبور خود میتوانید از منوی متفرقه، زیرمنوی ویرایش پروفایل اقدام کنید.\n
+				""");
+		
+	}
+}
