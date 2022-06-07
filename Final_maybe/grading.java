7ffce650f3e99d5e71f47eaa56b726d46ec94e7b
am-shm <am.shahmoradi93@gmail.com>
2022-06-07 13:43:29 +0430

    grading form base developed

---
+++ Final_maybe/grading.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class grading {
+	public class gradingForm {
+		String name;
+		JLabel nameLabel;
+		JTextField markField;
+	}
+	public static void main(String args[]){
+		int lxoff=50; // xoffset of lables 
+		int txoff=100; // xoffset of textfields
+		int yoff=50; // yoffset of lables and textfileds // will increase with loop index
+		String[] namesArr = {"ali","mohammad","sina"};
+		gradingForm[] studentForm = new gradingForm[3];
+		for (int )
+	}
+}
