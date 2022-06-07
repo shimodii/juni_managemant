c9a49d64195f7596ef98d2ed1151634ff754e532
am-shm <am.shahmoradi93@gmail.com>
2022-06-07 15:09:49 +0430

    objects automation maded

--- Final_maybe/grading.java
+++ Final_maybe/grading.java
@@ -10,8 +10,13 @@ public class grading {
 		int lxoff=50; // xoffset of lables 
 		int txoff=100; // xoffset of textfields
 		int yoff=50; // yoffset of lables and textfileds // will increase with loop index
-		String[] namesArr = {"ali","mohammad","sina"};
-		gradingForm[] studentForm = new gradingForm[3];
-		for (int )
+		// store names from db to array
+		String[] namesArr = /*names from db*/ ;
+		gradingForm[] studentForm = new gradingForm[namesArr];
+		for (int i=0;i<studentForm.length;i++){
+			studentForm[i].name=namesArr[i];
+			studentForm[i].nameLabel.setText(studentForm[i].name);
+
+		}
 	}
 }
