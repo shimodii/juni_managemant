581103cae6032930905f2c285ccf2ce91b4d85cf
am-shm <am.shahmoradi93@gmail.com>
2022-06-09 19:50:13 +0430

    some f

---
+++ Final_maybe/studentMain.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public studentMain {
+	String studentName = "AmirMohammad";
+	public static void main(String args[]){
+		JFrame mainFrame = new JFrame("صفحه اصلی");
+		menuBar.student(mainFrame);
+		//header.showHeader(mainFrame,studentName);
+	}
+}
