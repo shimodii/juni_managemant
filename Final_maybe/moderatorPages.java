31c55ee86b27ccfbe76ce9cec59991a609c2855c
am-shm <am.shahmoradi93@gmail.com>
2022-06-12 13:19:30 +0430

    moderator ui base developed

---
+++ Final_maybe/moderatorPages.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class moderatorPages{
+	public static void createNews(JFrame mainFrame){
+		
+	}
+	public static void deleteUser(JFrame mainFrame, String id){
+	
+	}
+
+}
